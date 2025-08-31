b8228ee52adc4c8b313c198dcb6918d2659f98f4
Herve97 <herdiebakongo@gmail.com>
2025-08-31 14:05:33 +0100

    Refactor project structure and enhance basic functionality.

---
+++ test.js
@@ -0,0 +1,7 @@
+const { twoSum } = require("./twosum");
+
+console.log("Testing twoSum function:");
+console.log("Input: nums = [2, 7, 11, 15], target = 9");
+console.log("Output:", twoSum([11, 2, 15, 7], 9));
+console.log("Expected: [1, 3]");
+// console.log("Expected: [0, 1]");
